e4769741ed914143e2d478d500bcbc4b561ff810
Janaina Tannus <61756665+jmtannus@users.noreply.github.com>
2024-01-26 12:54:54 +0000

    alterando commonjs para typemodules

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-var express = require('express')
-const app = express()
-const port = 3000
-var cors = require('cors')
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+const corsOptions = {
+  origin: 'http://example.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 app.get('/', cors(corsOptions), (req, res, next) => {
-    const json = JSON.stringify({
-        nome: req.query.nome,
-        idade: req.query.idade
-    })
+  const json = JSON.stringify({
+    nome: req.query.nome,
+    idade: req.query.idade > 20 ? "sobreviventes!": "Quero café!"
+  });
   res.send(json)
-})
+});
 
 app.listen(port, () => {
   console.log(`Backend rodando no local e porta ${port}`)
-})
+});
 
-var corsOptions = {
-  origin: 'http://example.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
 
